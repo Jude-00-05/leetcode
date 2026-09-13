cb88fbfc0ab9c5979d79ba5ec15f3a4689ed94a2
Alwin Sunny Jude <alwinsunnyjude@gmail.com>
2026-09-13 16:28:02 +0530

    Time: 2 ms (93.07%) | Memory: 72.3 MB (17.04%) - LeetSync

---
+++ solutions/238-product-of-array-except-self/product-of-array-except-self.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int[] productExceptSelf(int[] nums) {
+        int leftProduct=1;
+        int[] res=new int[nums.length];
+        for(int i=0;i<nums.length;i++){
+            res[i]=leftProduct;
+            leftProduct*=nums[i];
+        }
+        int rightProduct=1;
+        for(int i=nums.length-1;i>=0;i--){
+            res[i]*=rightProduct;
+            rightProduct*=nums[i];
+        }
+        return res;
+    }
+}
