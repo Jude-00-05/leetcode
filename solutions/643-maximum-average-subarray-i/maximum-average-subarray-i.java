bb644089445b509e37366de1f0596e9750179d1a
Alwin Sunny Jude <alwinsunnyjude@gmail.com>
2026-09-14 00:51:29 +0530

    Time: 6 ms (11.80%) | Memory: 69.4 MB (88.02%) - LeetSync

---
+++ solutions/643-maximum-average-subarray-i/maximum-average-subarray-i.java
@@ -0,0 +1,27 @@
+class Solution {
+    public double findMaxAverage(int[] nums, int k) {
+        int left=0;
+        int right=0;
+        double res=Integer.MIN_VALUE;
+        double sum=0;
+        double avg=0;        
+        while(right<nums.length && left<=right)
+        {
+            
+            sum+=nums[right];
+            right++;
+            if(right-left>k){
+                sum-=nums[left];
+                left++;   
+            }
+            if(right-left==k){
+                avg=sum/k;
+                res=Math.max(avg,res);                
+            }
+            
+            
+
+        }
+        return res;
+    }
+}
