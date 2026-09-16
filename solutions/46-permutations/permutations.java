92fd93895b2b5cffd3146ab9346855202a4d3bf6
Alwin Sunny Jude <alwinsunnyjude@gmail.com>
2026-09-17 00:29:27 +0530

    Time: 2 ms (35.24%) | Memory: 45.7 MB (29.14%) - LeetSync

--- solutions/46-permutations/permutations.java
+++ solutions/46-permutations/permutations.java
@@ -1,31 +1,42 @@
-import java.util.*;
+class Solution {
 
-public class Solution {
     public List<List<Integer>> permute(int[] nums) {
+
         List<List<Integer>> result = new ArrayList<>();
-        backtrack(nums, new ArrayList<>(), result);
+
+        boolean[] used = new boolean[nums.length];
+
+        backtrack(nums, used, new ArrayList<>(), result);
+
         return result;
     }
 
-    private void backtrack(int[] nums, List<Integer> tempList, List<List<Integer>> result) {
-        if (tempList.size() == nums.length) {
-            result.add(new ArrayList<>(tempList));
+    private void backtrack(
+        int[] nums,
+        boolean[] used,
+        List<Integer> current,
+        List<List<Integer>> result) {
+
+        if (current.size() == nums.length) {
+
+            result.add(new ArrayList<>(current));
+
             return;
         }
 
-        for (int num : nums) {
-            if (tempList.contains(num)) continue;
-            tempList.add(num);
-            backtrack(nums, tempList, result);
-            tempList.remove(tempList.size() - 1);
-        }
-    }
+        for (int i = 0; i < nums.length; i++) {
 
-    // Example usage
-    public static void main(String[] args) {
-        Solution sol = new Solution();
-        int[] nums = {1, 2, 3};
-        List<List<Integer>> permutations = sol.permute(nums);
-        System.out.println(permutations);
+            if (used[i]) {
+                continue;
+            }
+
+            used[i] = true;
+            current.add(nums[i]);
+
+            backtrack(nums, used, current, result);
+
+            current.remove(current.size() - 1);
+            used[i] = false;
+        }
     }
 }
