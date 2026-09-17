27f3d050f448ffa1ae68436b510a4a273495d42f
Alwin Sunny Jude <alwinsunnyjude@gmail.com>
2026-09-17 17:40:39 +0530

    Time: 0 ms (100.00%) | Memory: 42.4 MB (94.31%) - LeetSync

---
+++ solutions/198-house-robber/house-robber.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int rob(int[] nums) {
+        int prev1=0;
+        int prev2=0;
+        for(int i:nums)
+        {
+            int current=Math.max(prev1,prev2+i);
+            prev2=prev1;
+            prev1=current;
+
+        }
+        return prev1;
+    }
+}
